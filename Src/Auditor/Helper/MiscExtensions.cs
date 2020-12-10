4a27eeaeb09475f1c32d6ef43d4e4333d72fdefd
Moshood Aminu <moshood.aminu@gmail.com>
2020-12-10 17:58:34 +1300

    netstandard support
    Moved framework to netstandard2.0 to support wider range of projects

---
+++ Src/Auditor/Helper/MiscExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Auditor.Helper
+{
+    public static class MiscExtensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null) => new HashSet<T>(source, comparer);
+    }
+}
